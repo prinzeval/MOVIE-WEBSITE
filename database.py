4259d1197c0175ca314508a530916978e5e9f034
valentine onuoha <120737637+prinzeval@users.noreply.github.com>
2024-06-27 13:40:38 +0300

    Update database.py
    removed my database connection details

--- database.py
+++ database.py
@@ -32,7 +32,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Vondabaic2020@localhost/movie_data"  # Replace with your database URL
+ # Replace with your database URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
